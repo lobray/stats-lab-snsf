efd34f98b7dc9b8bf326114eda8c37fba68182c3
chiagi <32568989+chiagi@users.noreply.github.com>
2018-04-17 14:24:11 +0200

    Multinomial External Reg accuracy

--- multinomial.R
+++ multinomial.R
@@ -1,6 +1,13 @@
 source("Cleaning Functions.R")
 source("Data for Regression.R")
 
+library(vcd) 
+library(corrplot)
+library(caTools)
+library(ROCR)
+library(pROC)
+library(car)
+
 external_regression_data<-prepare_data_external_log_regression(final.apps,final.external)
 external_regression_data$ProposalCombined <- round((external_regression_data$ScientificRelevance+
                                                       external_regression_data$Suitability)/2, 0)
@@ -63,3 +70,73 @@ pchisq(4.800679, fit7$edf-fit6$edf, lower=FALSE)
 
 
 
+
+ExternalMultiModel<- function(data=external_regression_data,Div="All",
+                               SplitRatio=0.8, cutoff=0.5){ 
+  
+  if (Div == "All"){ 
+    final.data<- data
+  }
+  else {
+    final.data<- subset(data,Division==Div, select = -(Division)) 
+  }
+  
+  
+  #spliting the data into train and test set
+  
+  if (SplitRatio<1){
+    split<-sample.split(final.data$ProposalCombined, SplitRatio = 0.8)
+    Train<-subset(final.data, split=="TRUE")
+    Test <-subset(final.data, split=="FALSE") 
+  } else {
+    Test<-Train<-final.data
+  }
+  
+  # fitting the model
+  
+  #ExternalModel<-function(Train,Test,cutoff){
+  
+  # Cutoff
+  cutoff<-cutoff
+  
+  # Optimize the model
+  
+  # full<- glm(IsApproved ~ .-(ProjectID), data=Train, family="binomial")
+  # Model  <-step(full, direction = "backward", trace=0)
+  # Model <- step(empty,scope=list(lower=empty,upper=full), direction="both",
+  #     trace=0)
+  Model <- polr(ProposalCombined ~ Gender + Division + InstType + IsContinuation + 
+                  log(AmountRequested)+PercentFemale+Gender:Division+Gender:PercentFemale, 
+                method="logistic",data=Train)
+
+  
+  
+  # Testing the Treshold
+  par(mfrow=c(1,2))
+  predictor<-predict(Model, Test, type="class")
+  # Unfortunately ROCR only support evaluation of binary classification tasks
+  # ROCRPred<- prediction(predictor,labels=Test$ProposalCombined)
+  # ROCRPerf<- performance(ROCRPred,"tpr","fpr")
+  # plot(ROCRPerf, colorize=TRUE, print.cutoffs.at=seq(0,1,by=0.1))
+  
+  # with respect to accuracy
+  # ROCRACC<- performance(ROCRPred,"acc")
+  # plot(ROCRACC)  
+  
+  # Confusion Matrices
+  AccTable<-table(ActualValue=Test$ProposalCombined,Prediction=predictor)
+  accuracy<-(sum(diag(AccTable))/sum(AccTable))
+  
+  # Return
+  print(paste("Regresion for External Reviews.   ", "Division: ", Div))
+  return(list(Model= summary(Model), 
+              #`Confidence Intervals`=confint(Model),
+              `Confusion Matrix`=AccTable,
+              `Percentage of data used for Training`=paste(SplitRatio*100,"%"),
+              Accuracy=paste(round(accuracy,2)*100,"%")))
+  
+  
+}
+
+Model_fitted <- ExternalMultiModel()
+Model_fitted
