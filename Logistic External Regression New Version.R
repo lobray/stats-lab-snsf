8a3c22dd2b408a4724f075f98bc27b9d806aa162
chiagi <32568989+chiagi@users.noreply.github.com>
2018-05-02 15:29:01 +0200

    updated version

--- Logistic External Regression New Version.R
+++ Logistic External Regression New Version.R
@@ -66,28 +66,28 @@ legend("topleft",bty="n", legend=c("Not Approved","Approved"),lty=2,col=c("red",
         kappa <- cohen.kappa(kappa.matrix, w=linear.weights)  # 0.5
         kappa
     # Pseudos-R^2
-        # 1
-          (PsR21<-1-fit$dev/fit$null)  #[1] 0.2730928
-        # 2
-          (PsR22<-(1-exp((fit$dev-fit$null)/1623))/(1-exp(-fit$null/1623))) #[1] 0.4198219
+        (PsR22<-(1-exp((fit$dev-fit$null)/1623))/(1-exp(-fit$null/1623))) #[1] 0.4198219
     
     
     # optimize model see if we can drop some variables
-      drop1(fit, test="Chisq")
+      drop1(Model, test="Chisq")
       
       # Removing interaction between ApplicantTrack and gender reduces the AIC the most
       # I will remove this variable
-      
-        Model<-update(fit,.~.-ApplicantTrack:Gender)      
+        Model<-update(Model,.~.-ApplicantTrack:Gender) 
+        Model<-update(Model,.~.-Division:Gender)   
+        Model<-update(Model,.~.-InstType)
+        Model<-update(Model,.~.-logAmount)
+        Model<-update(Model,.~.-PercentFemale:Gender)
+        Model<-update(Model,.~.-PreviousRequest)
         summary(Model)
+        # We stop before removing Gender
         # No more NA, but still very large variances
         
         # Pseudos-R^2
-        # 1
-        (PsR21<-1-Model$dev/Model$null)  #[1] 0.2719115
-        # 2
-        (PsR22<-(1-exp((Model$dev-Model$null)/1623))/(1-exp(-Model$null/1623))) #[1] 0.4183261
+        (PsR22<-(1-exp((Model$dev-Model$null)/1623))/(1-exp(-Model$null/1623))) #[1]  0.4165827
         # Although AIC is less, the pseudo r's are worst.
+        Model$formula
         
 # No matter what assumption of contrast we use the model is not good. I am guessing 
 # this is because we have a perfect separation problem:
@@ -110,8 +110,9 @@ legend("topleft",bty="n", legend=c("Not Approved","Approved"),lty=2,col=c("red",
         
 # Fit again with new definition of variables:
         
-        Model <- glm(IsApproved ~ .+Gender:Division+
-                     Gender:PercentFemale+Gender:ApplicantTrack,data=data, 
+        Model <- glm(IsApproved ~ ApplicantTrack + ScientificRelevance + 
+                       Suitability + PercentFemale + Age + Gender*Division + InstType*Division + IsContinuation + 
+                       Semester,data=data, 
                    family="binomial")
         summary(Model)
         # We have no NA's now, and the variance are reasonable
@@ -138,37 +139,30 @@ legend("topleft",bty="n", legend=c("Not Approved","Approved"),lty=2,col=c("red",
         
         kappa.matrix <- as.matrix(table(data$IsApproved,pred>=0.5))
         kappa <- cohen.kappa(kappa.matrix, w=linear.weights) 
-        kappa  # again 0.5
+        kappa  # again 0.51
         
         # Pseudos-R^2
-        # 1
-        (Ps2R21<-1-Model$dev/Model$null)  #0.2697538
-        (PsR21-Ps2R21)  # [1] 0.00215778 We have a sligthly worst proportion of deviance explained
-        # 2
-        (Ps2R22<-(1-exp((Model$dev-Model$null)/1623))/(1-exp(-Model$null/1623))) #[1] 0.4198219
-        (PsR22-Ps2R22)  #[1] 0.002738762
-        
+        (1-exp((Model$dev-Model$null)/1623))/(1-exp(-Model$null/1623)) #[1] 0.412507
+ 
         # optimize model
-        drop1(fit, test="Chisq")
-        
+        drop1(Model, test="Chisq")
+        Model <- update(Model, .~.-Age)
+        # Model <- update(Model, .~.-Suitability)
         # Again, Removing interaction between ApplicantTrack and gender reduces the AIC the most
         # I will remove this variable
         
-        Model<-update(Model,.~.-ApplicantTrack:Gender)      
+
         summary(Model)
         # Pseudos-R^2
-        # 1
-        (Ps2R21<-1-Model$dev/Model$null)  # [1] 0.2687348
-        # 2
-        (PsR22<-(1-exp((Model$dev-Model$null)/1623))/(1-exp(-Model$null/1623))) #[1] 0.4142912
+        1-exp((Model$dev-Model$null)/1623))/(1-exp(-Model$null/1623)) #[1] 0.4142912
         # Although AIC is less, the pseudo r's are worst. But is to be expected, as the more
         # Variables the less the deviance, and therfore, the less proportion explained
       
         # In the following I will work with this final model
         Model$call
-        # glm(formula = IsApproved ~ ApplicantTrack + ScientificRelevance + 
-        # Suitability + PercentFemale + Age + Gender + Division + IsContinuation + 
-        # PreviousRequest + InstType + Semester + logAmount + Gender:Division, 
+        # glm(formula = IsApproved ~ ApplicantTrack + ScientificRelevance + Suitability + 
+        # PercentFemale + Age + Gender + Division + IsContinuation + 
+        #  Semester, 
         # family = "binomial", data = data)
        
 # Effect Visualization ----------------------------------------------------
@@ -183,15 +177,12 @@ library(ggplot2)
   eff.fit <- allEffects(Model)
   
 # Obtain Gender effect as a separate data frame        
-  GendDivEff<-as.data.frame(eff.fit[[11]])               
+  GendDivEff<-as.data.frame(eff.fit[[6]])               
 
 # Plot different things
   #Global Gender effect on the approval of applications  
   plot(Effect(focal.predictors = c("Gender"), Model),
      rug=FALSE,style="stacked")
-  
-  # Effect per Gender, on Is Approved by Division
-    plot(eff.fit[[11]])
   
   # Effect of gender in approval, per division
     plot(predictorEffects(Model, ~ Gender))
@@ -207,9 +198,13 @@ library(ggplot2)
 
 # Plots Gender effect accross divisions 
 # another way, still not good Anz ideas to make plots look nicer?
-# ggplot(GendDivEff,aes(x=Gender, y=fit, col=factor(Division)))+
-#   geom_line()
-  
+
+ggplot(GendDivEff[,which(GendDivEff$Division=="Div 1")],aes(x=Gender, y=fit))+
+  geom_line()
+
+ggplot(GendDivEff, aes(x=Gender, y=fit)) + 
+  geom_segment(data=GendDivEff, mapping=aes(x=Gender, y=lower, xend=Gender, yend=upper, colour=Division)) 
+
 
 # Relative variable importance --------------------------------------------
 
@@ -221,64 +216,16 @@ calc_pseudo_r <- function(Model,n) {
 # Area under the curve. For logistic regression this makes sense as a meassure for the variable 
 # importance. Here a code to calculate it taken from 
 
-GetROC_AUC = function(probs, true_Y){
-  # AUC approximation
-  # http://stackoverflow.com/questions/4903092/calculate-auc-in-r
-  # ty AGS
-  probsSort = sort(probs, decreasing = TRUE, index.return = TRUE)
-  val = unlist(probsSort$x)
-  idx = unlist(probsSort$ix) 
-  
-  roc_y = true_Y[idx];
-  stack_x = cumsum(roc_y == 0)/sum(roc_y == 0)
-  stack_y = cumsum(roc_y == 1)/sum(roc_y == 1)   
-  
-  auc = sum((stack_x[2:length(roc_y)]-stack_x[1:length(roc_y)-1])*stack_y[2:length(roc_y)])
-  return(auc)
-}
-
 # Set Predictors and output variables
   outcomeName <- 'IsApproved'
-  predictorNames <- setdiff(names(data),outcomeName)
+  predictorNames <- setdiff(c('ApplicantTrack','ScientificRelevance', 'Suitability',
+                              'PercentFemale','Age','Gender','Division','InstType',
+                              'IsContinuation', 'Semester'),outcomeName)
 
-# Predictions
-  predictions <- predict(object=Model, data[,predictorNames], type="response")
-  
-# Reference AUC approximation
-  refAUC <- GetROC_AUC(predictions,data[,outcomeName])
-  print(paste('AUC score:', refAUC))
-  
-# Shuffle predictions for variable importance
-  AUCShuffle <- NULL
-  shuffletimes <- 100  #number of interactions
-  
-  featuresMeanAUCs <- c()
-  for (feature in predictorNames) {
-    featureAUCs <- c()
-    shuffledData <- data[,predictorNames]
-    for (iter in 1:shuffletimes) {
-      shuffledData[,feature] <- sample(shuffledData[,feature], length(shuffledData[,feature]))
-      predictions <- predict(object=Model, shuffledData[,predictorNames], type="response")
-      featureAUCs <- c(featureAUCs,GetROC_AUC(predictions, data[,outcomeName]))
-    }
-    featuresMeanAUCs <- c(featuresMeanAUCs, mean(featureAUCs < refAUC))
-  }  
-  
-  AUCShuffle <- data.frame('feature'=predictorNames, 'importance'=featuresMeanAUCs)
-  AUCShuffle <- AUCShuffle[order(AUCShuffle$importance, decreasing=TRUE),]
-  print(AUCShuffle)
-  # Equally important are ApplicantTrack, Scientific Relevance, Suitability, Is continuation
-  # Follow by InstType, Gender Division, and so on
-  
-  # This graph needs some work
-  barplot(AUCShuffle[,2], names.arg = AUCShuffle[,1],horiz = TRUE, 
-          col=rainbow(30),
-          main="Relative Importance of the Variables")
-  
 # To see variable importance based on pseudo r^2
   refR2<-calc_pseudo_r(Model,nrow(data))
   PseudoRShuffle <- NULL
-  shuffletimes <- 100  #number of interactions
+  shuffletimes <- 50  #number of interactions
   
   featuresMeanR2 <- c()
   for (feature in predictorNames) {
@@ -289,22 +236,9 @@ GetROC_AUC = function(probs, true_Y){
       Model.tmp <- update(Model,.~.,data=shuffledData)
       featureR2 <- c(featureR2,calc_pseudo_r(Model.tmp,nrow(shuffledData)))
     }
-    featuresMeanR2 <- c(featuresMeanR2, mean(featureR2 < refR2))
+    featuresMeanR2 <- c(featuresMeanR2, mean(featureR2-refR2))
   }  
   
   PseudoRShuffle <- data.frame('feature'=predictorNames, 'importance'=featuresMeanR2)
   PseudoRShuffle <- PseudoRShuffle[order(PseudoRShuffle$importance, decreasing=TRUE),]
   print(PseudoRShuffle)
-
-  # To compare both methods
-    Comp<- cbind(AUC=AUCShuffle[,2],PseudoR2=PseudoRShuffle[,2])
-    row.names(Comp)<-AUCShuffle[,1]
-    
-    
-# Last approach for looking at the variable importance, using caret::varImp
-    library(caret)
-    C.varImp<- data.frame(varImp(Model))
-   
-    
-    
-  
