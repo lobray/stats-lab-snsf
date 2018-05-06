2e48fefa9a3dea1e6659513ab3cbbea7a2fdd2e5
carlasc <carlasc@student.ethz.ch>
2018-05-06 15:38:01 +0200

    graphs for presentation

---
+++ perfect separation graphs.R
@@ -0,0 +1,82 @@
+##############################################################
+#######   Graphs to show perfect separation
+#############################################################
+
+
+
+
+data<-prepare_data_external_log_regression(final.apps,final.external)
+
+
+# visualization of grades to check for perfect separation----
+
+#ApplicantTrack
+
+r.tab<-table(data$ApplicantTrack, data$IsApproved)
+r.tab.dataframe <- as.data.frame(r.tab)    
+colnames(r.tab.dataframe) <- c("ApplicantTrack", "IsApproved", "Freq")
+
+
+ggplot(r.tab.dataframe,aes(x=ApplicantTrack,y=Freq,fill=factor(IsApproved)))+
+  geom_bar(stat="identity",position="dodge")+
+  scale_fill_discrete(name="ApplicantTrack",
+                      breaks=c(1, 2),
+                      labels=c( "Approved","Not Approved"))+
+  ylab("Frequency") +xlab("")+
+  ggtitle("External Step Grade distribution")+
+  theme(legend.position="bottom")
+
+
+#ProposalCombined
+
+r.tab<-table(data$ProposalCombined, data$IsApproved)
+r.tab.dataframe <- as.data.frame(r.tab)    
+colnames(r.tab.dataframe) <- c("ProposalCombined", "IsApproved", "Freq")
+
+
+ggplot(r.tab.dataframe,aes(x=ProposalCombined,y=Freq,fill=factor(IsApproved)))+
+  geom_bar(stat="identity",position="dodge")+
+  scale_fill_discrete(name="ProposalCombined",
+                      breaks=c(1, 2),
+                      labels=c( "Approved","Not Approved"))+
+  ylab("Frequency") +xlab("")+
+  ggtitle("External Step Grade distribution")+
+  theme(legend.position="bottom")
+
+
+# Internal graphs
+data<-prepare_data_internal_log_regression(final.apps,final.internal)
+
+#ApplicantTrack
+
+r.tab<-table(data$ApplicantTrack, data$IsApproved)
+r.tab.dataframe <- as.data.frame(r.tab)    
+colnames(r.tab.dataframe) <- c("ApplicantTrack", "IsApproved", "Freq")
+
+
+ggplot(r.tab.dataframe,aes(x=ApplicantTrack,y=Freq,fill=factor(IsApproved)))+
+  geom_bar(stat="identity",position="dodge")+
+  scale_fill_discrete(name="ApplicantTrack",
+                      breaks=c(1, 2),
+                      labels=c( "Approved","Not Approved"))+
+  ylab("Frequency") +xlab("")+
+  ggtitle("Internal Step Grade distribution")+
+  theme(legend.position="bottom")
+
+
+#ProjectAssessment
+
+r.tab<-table(data$ProjectAssessment, data$IsApproved)
+r.tab.dataframe <- as.data.frame(r.tab)    
+colnames(r.tab.dataframe) <- c("ProjectAssessment", "IsApproved", "Freq")
+
+
+ggplot(r.tab.dataframe,aes(x=ProjectAssessment,y=Freq,fill=factor(IsApproved)))+
+  geom_bar(stat="identity",position="dodge")+
+  scale_fill_discrete(name="ProjectAssessment",
+                      breaks=c(1, 2),
+                      labels=c( "Approved","Not Approved"))+
+  ylab("Frequency") +xlab("")+
+  ggtitle("Internal Step Grade distribution")+
+  theme(legend.position="bottom")
+
